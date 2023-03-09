20b7f6f4b68f17c552111a9bda672b9824222c6f
prabhat-3 <72614694+Aditya-2bharti@users.noreply.github.com>
2023-03-09 12:32:23 +0530

    Table

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   
+  <HomePage  data = 'This is aditya' msg="hello from the aditya"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    HomePage
   }
 }
 </script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1 v-if="show">Hello from home component is true</h1>
+        <h1 v-else>Hello from home component is false</h1>
+        <!-- <h2>{{ data }}</h2>
+        <h2>{{ msg }}</h2> -->
+        <!-- <button v-on:click="hello"  >count method</button> -->
+        <button v-on:click="display">Toggle Element</button>
+
+
+        <h1>This is For Loop</h1>
+        <table border="1">
+            <tr>
+                <td>id</td>
+                <td>Name</td>
+                <td>Gmail</td>
+            </tr>
+            <tr  v-for="item in users" :key="item.id">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.gmail}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+ 
+ 
+
+export default {
+    name: 'HomePage',
+    props: {
+        data: String,
+        msg: String,
+    },
+    // methods:{
+    //     hello(){
+    //         console.log("this is method");      
+    //     }
+    // },
+    // data() {
+    //     return { show: true }
+    // },
+    methods: {
+        display() {
+            this.show = !this.show;
+        }
+    },
+    data() {
+        return {
+            users: [
+                {id:1,name:'Aditya',gmail:'aditya@test.com'},
+                {id:2,name:'kumar',gmail:'kumar@test.com'},
+                {id:3,name:'rom',gmail:'rom@test.com'},
+                {id:4,name:'tony',gmail:'tony@test.com'},
+            ]
+        }
+    }
+}
+</script>
